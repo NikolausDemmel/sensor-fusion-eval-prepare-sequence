0879ced132c1a0d324edb96ce35d02a8196998c5
Nikolaus Demmel <nikolaus@nikolaus-demmel.de>
2013-06-11 11:25:56 +0200

    updates

--- mk/prepare.mk
+++ mk/prepare.mk
@@ -19,25 +19,30 @@ START_PS:=$(shell bc <<< "$(START_TIME)-1")
 DURATION_PS:=$(shell bc <<< "$(DURATION)+2")
 
 
-.PHONY: all clean killros
+.PHONY: all build1 build2 clean killros
+.SECONDARY:
 
-all: ps.bag matched.bag
+all: build1 build2
+
+build1: ps1_margin.txt ps1_margin.bag ps1.txt ps1.bag matched1.bag unmatched1.bag 
+build2: ps2_margin.txt ps2_margin.bag ps2.txt ps2.bag matched2.bag unmatched2.bag 
+
 
 clean:
-	rm  *.log *.txt tf.bag ps*.bag *margin.bag *matched*.bag measurementinfocamera.bag  || true
+	-rm  *.log *.txt tf*.bag ps*.bag *margin.bag *matched*.bag measurementinfocamera*.bag
 
 killros:
 	killall -2 record || true
 	killall roslaunch || true
 	killall roscore || true
 
-tf.bag: recorded.bag 
+tf%.bag: recorded.bag 
 	[ 0 == `ps aux | grep "\(r\)osmaster" | wc -l` ] || (echo "Error: roscore is running!" && false) # hacky way to test if roscore is running
 	roscore &
 	sleep 3
-	roslaunch ../config/handheld_with_phasespace.launch &
+	roslaunch ../config/handheld_with_phasespace$*.launch &
 	sleep 1
-	rosbag record -O tf.bag /tf &
+	rosbag record -O $@ /tf &
 	sleep 1
 	roslaunch uvm_handheld_evaluation rosbag_play.launch bag:=recorded.bag topics:="/tf" rate:=5
 	killall -2 record
@@ -46,33 +51,33 @@ tf.bag: recorded.bag
 	echo "Waiting for shutdown..."
 	sleep 3
 	echo "... complete."
-	chmod a-w tf.bag # pretect from accidental deletion
+	chmod a-w $@ # pretect from accidental deletion
 
-ps.txt: tf.bag
-	rosrun rgbd_benchmark_tools extract_poses_from_bag.py -i tf.bag -o ps.txt -t /tf -f /map -c /ps/camera_uvm_optical_frame | tee ps.txt.log
+ps%_margin.bag: ps%.bag
+	rosrun uvm_handheld_evaluation rosbag_filter_subsequence.sh $< $@ $(START_PS) $(DURATION_PS) | tee $@.log
 
-ps.bag: ps.txt
-	rosrun rgbd_benchmark_tools trajectory_txt2bag.py ps.txt ps.bag -t /ps | tee ps.bag.log
+ps%_margin.txt: ps%_margin.bag
+	rosrun rgbd_benchmark_tools extract_poses_from_bag.py -i $< -o $@ -t /ps
 
-ps_margin.bag: ps.bag
-	rosrun uvm_handheld_evaluation rosbag_filter_subsequence.sh ps.bag ps_margin.bag $(START_PS) $(DURATION_PS) | tee ps_margin.log
+ps%.txt: tf%.bag
+	rosrun rgbd_benchmark_tools extract_poses_from_bag.py -i $< -o $@ -t /tf -f /map -c /ps/camera_uvm_optical_frame | tee $@.log
 
-ps_margin.txt: ps_margin.bag
-	rosrun rgbd_benchmark_tools extract_poses_from_bag.py -i ps_margin.bag -o ps_margin.txt -t /ps
+ps%.bag: ps%.txt
+	rosrun rgbd_benchmark_tools trajectory_txt2bag.py $< $@ -t /ps | tee $@.log
 
 richimage_margin.bag: richimage.bag
-	rosrun uvm_handheld_evaluation rosbag_filter_subsequence.sh richimage.bag richimage_margin.bag $(START_IMAGE) $(DURATION_IMAGE) | tee richimage_margin.log
+	rosrun uvm_handheld_evaluation rosbag_filter_subsequence.sh richimage.bag richimage_margin.bag $(START_IMAGE) $(DURATION_IMAGE) | tee $@.log
 
 imu_margin.bag: imu.bag
-	rosrun uvm_handheld_evaluation rosbag_filter_subsequence.sh imu.bag imu_margin.bag $(START_IMU) $(DURATION_IMU) | tee imu_margin.log
+	rosrun uvm_handheld_evaluation rosbag_filter_subsequence.sh imu.bag imu_margin.bag $(START_IMU) $(DURATION_IMU) | tee $@.log
 
-unmatched.bag: richimage_margin.bag tf.bag imu_margin.bag
-	rosrun uvm_handheld_evaluation rosbag_merge.py unmatched.bag richimage_margin.bag tf.bag imu_margin.bag --verbose | tee unmatched.log
+unmatched%.bag: richimage_margin.bag tf%.bag imu_margin.bag
+	rosrun uvm_handheld_evaluation rosbag_merge.py $@ richimage_margin.bag tf$*.bag imu_margin.bag --verbose | tee $@.log
 
-measurementinfocamera.bag: unmatched.bag
-	rosrun vps_bag_tools bag_location_estimator -i unmatched.bag -o measurementinfocamera.bag -d true -s ../config/c.xml -m ../config/m1.map | tee measurementinfocamera.log
-	chmod a-w measurementinfocamera.bag # protect from accidental deletion
-	chmod a-w measurementinfocamera.log # protect from accidental deletion
+measurementinfocamera%.bag: unmatched%.bag
+	rosrun vps_bag_tools bag_location_estimator -i $< -o $@ -d true -s ../config/c.xml -m ../config/m1.map | tee $@.log
+	chmod a-w $@     # protect from accidental deletion
+	chmod a-w $@.log # protect from accidental deletion
 
-matched.bag: measurementinfocamera.bag imu_margin.bag tf.bag
-	rosrun uvm_handheld_evaluation rosbag_merge.py matched.bag imu_margin.bag tf.bag measurementinfocamera.bag -t "*camera_measurement_info *imu* *tf" --verbose | tee matched.log
+matched%.bag: measurementinfocamera%.bag imu_margin.bag tf%.bag
+	rosrun uvm_handheld_evaluation rosbag_merge.py $@ imu_margin.bag tf$*.bag measurementinfocamera$*.bag -t "*camera_measurement_info *imu* *tf" --verbose | tee $@.log
